92704f163f2ccb10f6ba7eb30d471c661f2c7ce0
Ben Cynamon <ben.cynamon@gmail.com>
2019-04-22 21:17:42 -0400

    Updates

---
+++ precache-manifest.835412ed078bfd308ba15ef45fd67f91.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "eb15bcaaedff49db10a8",
+    "url": "/react-youtube-api/static/css/main.36e673c4.chunk.css"
+  },
+  {
+    "revision": "eb15bcaaedff49db10a8",
+    "url": "/react-youtube-api/static/js/main.5dd88da5.chunk.js"
+  },
+  {
+    "revision": "f8054ee103191bcbf708",
+    "url": "/react-youtube-api/static/js/runtime~main.ef247ad5.js"
+  },
+  {
+    "revision": "381bd96f99f3fb8fb88d",
+    "url": "/react-youtube-api/static/js/2.c79a505c.chunk.js"
+  },
+  {
+    "revision": "2c246de8e1d70e3c550dfa3f2360716a",
+    "url": "/react-youtube-api/index.html"
+  }
+];
